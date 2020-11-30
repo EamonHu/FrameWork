94610612254c1aa6cbaab6c4722295e84652ac97
Eamon <hym_95510@163.com>
2020-11-30 21:59:49 +0800

    create project

---
+++ mybatis-demo/src/main/java/study/eamon/dao/UserDao.java
@@ -0,0 +1,19 @@
+package study.eamon.dao;
+
+import study.eamon.po.User;
+
+import java.util.List;
+
+/**
+ * @Author: Eamon
+ * @Description:
+ * @Date: 2020/11/23 21:22
+ */
+
+public interface UserDao {
+    User findUserById(int id) throws Exception;
+
+    List<User> findUserByName(String name) throws  Exception;
+
+    void insertUser(User user) throws Exception;
+}
