4fcb9ebb24c8f14802c8750bf983d8b1cd60c6ef
Eamon <hym_95510@163.com>
2020-12-17 15:14:00 +0800

    [add]新增springbootJwt中的JWT教程.pdf/pom.xml

---
+++ springbootjwt/src/main/java/cn/eamon/SpringbootjwtApplication.java
@@ -0,0 +1,17 @@
+package cn.eamon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+ /**
+   * @Author: Eamon
+   * @Description:
+   * @Date: 2020/12/16 17:21
+   */
+@SpringBootApplication
+public class SpringbootjwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootjwtApplication.class, args);
+	}
+}
