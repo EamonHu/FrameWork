b675cd6dc2e893e811c105dcd6d6b02fd78b12bb
Eamon <hym_95510@163.com>
2021-01-11 10:02:26 +0800

    [add]添加Springboot组件

---
+++ springboot/springboot_01_start/src/main/java/cn/eamon/Springboot01StartApplication.java
@@ -0,0 +1,13 @@
+package cn.eamon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot01StartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot01StartApplication.class, args);
+	}
+
+}
---
+++ springboot/springboot_01_start/src/test/java/cn/eamon/Springboot01StartApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.eamon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot01StartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
